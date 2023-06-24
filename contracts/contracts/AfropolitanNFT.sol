ec5776ef80b388f285e2979d88c3f94671e12228
Nicholas Offer <noffer@live.com>
2023-06-24 17:25:34 -0400

    afro update

--- contracts/contracts/AfropolitanNFT.sol
+++ contracts/contracts/AfropolitanNFT.sol
@@ -8,18 +8,23 @@ import "openzeppelin-contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract AfropolitanNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
+    string public baseURI;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("Afropolitan Citizen", "AFROPOL") {}
+    constructor(
+        string memory _baseURI,
+    ) ERC721("Afropolitan Citizen", "AFROPOL") {
+        setBaseURI(_baseURI);
+    }
 
     function mintNFT(
-        address recipient,
-        string memory tokenURI
+        address recipient
     ) public onlyOwner returns (uint256) {
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
+        string tokenURI = string.concat(baseURI, '/', Strings.toString(_tokenIds.current()))
         _setTokenURI(newItemId, tokenURI);
 
         return newItemId;
