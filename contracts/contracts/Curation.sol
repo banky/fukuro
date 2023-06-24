383e923cc9ac341f3d07e92d44c77cd0e62ebd98
straightupjac <jdc.jaclyn@gmail.com>
2023-06-23 23:56:09 -0400

    --amend

--- contracts/contracts/Curation.sol
+++ contracts/contracts/Curation.sol
@@ -1,76 +1,77 @@
-pragma solidity ^0.8.13;
+// pragma solidity ^0.8.13;
 
-import "openzeppelin-contracts/utils/introspection/IERC165.sol";
-import "openzeppelin-contracts/token/ERC721/IERC721.sol";
-import "openzeppelin-contracts/interfaces/IERC1271.sol";
-import "openzeppelin-contracts/utils/cryptography/SignatureChecker.sol";
-import "sstore2/utils/Bytecode.sol";
+// import "@openzeppelin/utils/introspection/IERC165.sol";
+// import "@openzeppelin/token/ERC721/IERC721.sol";
+// import "@openzeppelin/interfaces/IERC1271.sol";
+// import "@openzeppelin/utils/cryptography/SignatureChecker.sol";
+// import "sstore2/utils/Bytecode.sol";
+// import './IERC6551Account.sol';
 
-contract ExampleERC6551Account is IERC165, IERC1271, IERC6551Account {
-    receive() external payable {}
+// contract ExampleERC6551Account is IERC165, IERC1271, IERC6551Account {
+//     receive() external payable {}
 
-    function executeCall(
-        address to,
-        uint256 value,
-        bytes calldata data
-    ) external payable returns (bytes memory result) {
-        require(msg.sender == owner(), "Not token owner");
+//     function executeCall(
+//         address to,
+//         uint256 value,
+//         bytes calldata data
+//     ) external payable returns (bytes memory result) {
+//         require(msg.sender == owner(), "Not token owner");
 
-        bool success;
-        (success, result) = to.call{value: value}(data);
+//         bool success;
+//         (success, result) = to.call{value: value}(data);
 
-        if (!success) {
-            assembly {
-                revert(add(result, 32), mload(result))
-            }
-        }
-    }
+//         if (!success) {
+//             assembly {
+//                 revert(add(result, 32), mload(result))
+//             }
+//         }
+//     }
 
-    function token()
-        external
-        view
-        returns (
-            uint256 chainId,
-            address tokenContract,
-            uint256 tokenId
-        )
-    {
-        uint256 length = address(this).code.length
-        return
-            abi.decode(
-                Bytecode.codeAt(address(this), length - 0x60, length),
-                (uint256, address, uint256)
-            );
-    }
+//     function token()
+//         external
+//         view
+//         returns (
+//             uint256 chainId,
+//             address tokenContract,
+//             uint256 tokenId
+//         )
+//     {
+//         uint256 length = address(this).code.length
+//         return
+//             abi.decode(
+//                 Bytecode.codeAt(address(this), length - 0x60, length),
+//                 (uint256, address, uint256)
+//             );
+//     }
 
-    function owner() public view returns (address) {
-        (uint256 chainId, address tokenContract, uint256 tokenId) = this
-            .token();
-        if (chainId != block.chainid) return address(0);
+//     function owner() public view returns (address) {
+//         (uint256 chainId, address tokenContract, uint256 tokenId) = this
+//             .token();
+//         if (chainId != block.chainid) return address(0);
 
-        return IERC721(tokenContract).ownerOf(tokenId);
-    }
+//         return IERC721(tokenContract).ownerOf(tokenId);
+//     }
 
-    function supportsInterface(bytes4 interfaceId) public pure returns (bool) {
-        return (interfaceId == type(IERC165).interfaceId ||
-            interfaceId == type(IERC6551Account).interfaceId);
-    }
+//     function supportsInterface(bytes4 interfaceId) public pure returns (bool) {
+//         return (interfaceId == type(IERC165).interfaceId ||
+//             interfaceId == type(IERC6551Account).interfaceId);
+//     }
 
-    function isValidSignature(bytes32 hash, bytes memory signature)
-        external
-        view
-        returns (bytes4 magicValue)
-    {
-        bool isValid = SignatureChecker.isValidSignatureNow(
-            owner(),
-            hash,
-            signature
-        );
+//     function isValidSignature(bytes32 hash, bytes memory signature)
+//         external
+//         view
+//         returns (bytes4 magicValue)
+//     {
+//         bool isValid = SignatureChecker.isValidSignatureNow(
+//             owner(),
+//             hash,
+//             signature
+//         );
 
-        if (isValid) {
-            return IERC1271.isValidSignature.selector;
-        }
+//         if (isValid) {
+//             return IERC1271.isValidSignature.selector;
+//         }
 
-        return "";
-    }
-}
+//         return "";
+//     }
+// }
