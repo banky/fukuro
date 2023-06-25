3e863d10bcb916ca8c140c4a6565a44d1d6829a9
banky <bankiadebajo1@gmail.com>
2023-06-25 08:39:16 -0400

    Remove export from page

--- frontend/src/app/buy/[auctionAddress]/page.tsx
+++ frontend/src/app/buy/[auctionAddress]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import {ethers} from 'ethers';
+import { ethers } from "ethers";
 import { useContext, useEffect, useMemo, useState } from "react";
 import { Button } from "../../../components/Button";
 import { usePathname } from "next/navigation";
@@ -13,37 +13,63 @@ import { SiOpensea } from "react-icons/si";
 import { BiArrowBack } from "react-icons/bi";
 import Link from "next/link";
 import { AUCTIONS, OPENSEA_URL } from "../../../utils/constants";
-import { Auction, HydratedAuction, hydrateAuction } from "../../../components/Auction";
+import {
+  Auction,
+  HydratedAuction,
+  hydrateAuction,
+} from "../../../components/Auction";
 import { NFTList } from "../../../components/NFTList";
 import { AuctionsContext } from "../../../contexts/AuctionsContext";
 import { fetchBlockNumber } from "wagmi/actions";
 
-export function Page() {
-  const {activeAuctions, inactiveAuctions, loading} = useContext(AuctionsContext);
+function Page() {
+  const { activeAuctions, inactiveAuctions, loading } = useContext(
+    AuctionsContext
+  );
   const pathName = usePathname();
   const auctionAddress = pathName.replace("/buy/", "");
 
-  const auction = useMemo(()=>[...activeAuctions, ...inactiveAuctions].find((auction) => auction.auctionAddress === auctionAddress),[activeAuctions, inactiveAuctions, auctionAddress])
-  const [startTimestampEstimate, setStartTimestampEstimate] = useState(BigInt(0));
+  const auction = useMemo(
+    () =>
+      [...activeAuctions, ...inactiveAuctions].find(
+        (auction) => auction.auctionAddress === auctionAddress
+      ),
+    [activeAuctions, inactiveAuctions, auctionAddress]
+  );
+  const [startTimestampEstimate, setStartTimestampEstimate] = useState(
+    BigInt(0)
+  );
   const [endTimestampEstimate, setEndTimestampEstimate] = useState(BigInt(0));
 
   useEffect(() => {
-    if (!auction) { return; }
+    if (!auction) {
+      return;
+    }
     const getAuctionData = async () => {
       const block = await fetchBlockNumber();
       if (block < auction.startBlock) {
-        setStartTimestampEstimate(BigInt(Date.now()) + (BigInt(auction.startBlock) - block)*BigInt(12)*BigInt(1000))
+        setStartTimestampEstimate(
+          BigInt(Date.now()) +
+            (BigInt(auction.startBlock) - block) * BigInt(12) * BigInt(1000)
+        );
       }
-      const provider = new ethers.providers.JsonRpcProvider(process.env.NEXT_PUBLIC_GOERLI_RPC_URL)
-      const startTimestamp = Number((await provider.getBlock(Number(auction.startBlock))).timestamp);
+      const provider = new ethers.providers.JsonRpcProvider(
+        process.env.NEXT_PUBLIC_GOERLI_RPC_URL
+      );
+      const startTimestamp = Number(
+        (await provider.getBlock(Number(auction.startBlock))).timestamp
+      );
       const blockDiff = auction.endBlock - auction.startBlock;
-      setStartTimestampEstimate(BigInt(startTimestamp)*BigInt(1000))
-      setEndTimestampEstimate(BigInt(blockDiff) * BigInt(12) * BigInt(1000) + BigInt(startTimestamp)*BigInt(1000))
-    }
+      setStartTimestampEstimate(BigInt(startTimestamp) * BigInt(1000));
+      setEndTimestampEstimate(
+        BigInt(blockDiff) * BigInt(12) * BigInt(1000) +
+          BigInt(startTimestamp) * BigInt(1000)
+      );
+    };
     if (!loading) {
-      getAuctionData()
+      getAuctionData();
     }
-  },[setEndTimestampEstimate, auction, loading])
+  }, [setEndTimestampEstimate, auction, loading]);
 
   return (
     <div className=" min-h-screen">
@@ -56,7 +82,15 @@ export function Page() {
           </Link>
           <h1 className="text-center text-2xl">Auction for {auctionAddress}</h1>
           <NFTList />
-          {loading ? "Loading..." : <Auction auction={auction} startTimestampEstimate={startTimestampEstimate} endTimestampEstimate={endTimestampEstimate}/>}
+          {loading ? (
+            "Loading..."
+          ) : (
+            <Auction
+              auction={auction}
+              startTimestampEstimate={startTimestampEstimate}
+              endTimestampEstimate={endTimestampEstimate}
+            />
+          )}
         </div>
       </div>
     </div>
